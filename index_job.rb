ff401f140e962a0121c05679706224202bf5535a
Garen Torikian <gjtorikian@gmail.com>
2014-07-11 09:04:26 -0700

    Try to fetch from a list of classes

--- index_job.rb
+++ index_job.rb
@@ -5,6 +5,13 @@ require './page'
 class IndexJob
   @queue = :default
 
+  CONTENT_MATCHES = [
+    'div[role="main"]',
+    'div.content',
+    'div.content-body',
+    'body'
+  ]
+
   def self.perform(tmpdir, repo)
     clone_repo(repo, tmpdir)
     Dir.chdir "#{tmpdir}/#{repo}" do
@@ -15,9 +22,18 @@ class IndexJob
 
         # TODO: make these configurable?
         doc = Nokogiri::HTML(html_file_contents)
-        body = doc.xpath("//body").text().strip
         title = doc.xpath("//title").text().strip
         last_updated = doc.xpath("//span[contains(concat(' ',normalize-space(@class),' '),'last-modified-at-date')]").text().strip
+
+        body = []
+
+        CONTENT_MATCHES.each do |content_selector|
+          body += doc.css(content_selector)
+          break if body.any?
+        end
+
+        # convert from Nokogiri Element objects to strings
+        body.map!(&:inner_text)
 
         page = Page.new id: "#{repo}::#{relative_path}", title: title, body: body, path: relative_path, last_updated: last_updated
 
